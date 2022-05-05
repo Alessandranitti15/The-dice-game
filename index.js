5779b057a1bf5c069da7ece201ccdeac8f862f3d
Alessandranitti15 <101338498+Alessandranitti15@users.noreply.github.com>
2022-05-05 14:08:52 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+
+var randomNumber1 = Math.floor(Math.random()*6+1);
+
+var randomNumber2 = Math.floor(Math.random()*6+1);
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").textContent = "Player 1 wins!";
+} else if (randomNumber1 < randomNumber2) {
+  document.querySelector("h1").textContent = "Player 2 wins!";
+} else {
+  document.querySelector("h1").textContent = "Draw!";
+}
+
+var randomImg = "styles.css/images/dice" + randomNumber1 + ".png";
+
+var randomImg2 = "styles.css/images/dice" + randomNumber2 + ".png";
+
+var image1 = document.querySelectorAll("img")[0].setAttribute("src", randomImg);
+
+var image2 = document.querySelectorAll("img")[1].setAttribute("src", randomImg2);
